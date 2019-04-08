6335a0c41c52198e70e83ab01635cd37c30de593
Kelm <thorsten.kelm@hs-bochum.de>
2019-04-08 11:33:08 +0200

    Initial main script

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Template.py
+# Author: Thorsten Kelm
+# thorsten.kelm@hs-bochum.de
+
+# Entry point for the script
+
+
+def main():
+    print("python main function")
+
+
+if __name__ == '__main__':
+    main()
