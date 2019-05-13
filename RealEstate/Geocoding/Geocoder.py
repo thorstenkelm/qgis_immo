b5cb79811cfa88f1c5a905a5c63ef38bc38b9c68
Fabian Hannich <fabian.hannich@hs-bochum.de>
2019-05-13 10:09:26 +0200

    Merge branch 'master' of C:\Users\Fabian Hannich\Documents\Studium\6. Semester\GI-Projekt_Immo\GitHub-Projekt with conflicts.

--- RealEstate/Geocoding/Geocoder.py
+++ RealEstate/Geocoding/Geocoder.py
@@ -7,15 +7,16 @@ import geocoder
 from pyproj import Proj, transform
 from dfply import *
 
+class Geocoder ():
 
-class Geocoder():
     # define transformation parameter
     WGS84 = Proj(init='epsg:4326')
     ETRS89 = Proj(init='epsg:4647')
 
-    def __init__(self, gebref):
+    def __init__(self, gebref, adresse):
         self.__gebref = gebref
-        self.__
+        self.__adresse = adresse
+
 
     # get Coordinates from an adress
     def getCoordinates(self, address):
@@ -30,66 +31,52 @@ class Geocoder():
             gebref = self.gebref >> mask(X.stn == a[0],
                                          X.hsr == a[1],
                                          X.adz == a[2])
+        coordinates = gebref.to_dict(orient='list')
+        x = coordinates['east']
+        y = coordinates['north']
+        accuracy = 1
+
+        return {"accuracy": accuracy,
+                "x": x,
+                "y": y}
+
 
         else:
             print("Geocode-function is used to get the coordinates..")
             return self.geocode(address)
 
-            coordinates = gebref.to_dict(orient='list')
-            x = coordinates['east']
-            y = coordinates['north']
-            accuracy = 1
 
-            return {"accuracy": accuracy,
-                    "x": x,
-                    "y": y}
+       #couter in eine eigene Funktion
+
 
     def geocode(self, address):
 
         # TODO "try-block" no address found - https://geocoder.readthedocs.io/api.html#examples Error Handling
         # TODO try-block ConnectionError - return is missing
-        try:
-            #Uhrzeitabfrage
-            # geocode address
-            g = geocoder.osm(address)
-            if g.ok:
 
-                json = g.geojson['features'][0]['properties']
+        # geocode address
+        g = geocoder.osm(address)
+        json = g.geojson['features'][0]['properties']
 
-                # extract information
-                accuracy = json['accuracy']
-                lat = json['lat']
-                lng = json['lng']
+        # extract information
+        accuracy = json['accuracy']
+        lat = json['lat']
+        lng = json['lng']
 
-                # convert wgs to etrs
-                x_trans, y_trans = self.wgs2etrs(lng, lat)
+        # convert wgs to etrs
+        x_trans, y_trans = self.wgs2etrs(lng, lat)
 
-                return self.return_data(accuracy,
-                                        x_trans,
-                                        y_trans)
-            else:
-                return self.return_data()
-
-
-        except ConnectionError as e:
-            print(e)
-            return self.return_data()
-
-    @staticmethod
-    def return_data(accuracy=0, x=0, y=0):
         return {"accuracy": accuracy,
-                "x": x,
-                "y": y}
+                "x": x_trans,
+                "y": y_trans}
+
 
     def wgs2etrs(self, lng, lat):
-        """
+        x_trans, y_trans = transform(self.WGS84, self.ETRS89, lng, lat)
+        return x_trans, y_trans
 
-        :param lng:
-        :param lat:
-        :return:
-        """
-        return transform(self.WGS84, self.ETRS89, lng, lat)
 
 if __name__ == '__main__':
-    # g = Geocoder(gebref=, adresse="Witteringstr 9")
+ # g = Geocoder(gebref=")
     pass
+
