94b865aa3f325c6b4569051fa502b8e3dbec2508
Fabian Hannich <fabian.hannich@hs-bochum.de>
2019-05-21 16:37:56 +0200

    Update class Geocoder

--- RealEstate/Geocoding/Geocoder.py
+++ RealEstate/Geocoding/Geocoder.py
@@ -2,7 +2,7 @@
 Description: Class to get the coordinates from
             the addresses. If the address does
             not exist in the ALKIS dataset,
-            you will get them about OpenStrretMap.
+            you will get them about OpenStreetMap.
 Author: Lena Karweg, lena.karweg@hs-bochum.de
         Fabian Hannich, fabian.hannich@hs-bochum.de
 """
@@ -12,6 +12,7 @@ from pyproj import Proj, transform
 from dfply import *
 from datetime import datetime as DateTime
 import time
+from RealEstate.Geocoding.BuildingReferences import BuildingReferences
 
 
 class Geocoder:
@@ -23,46 +24,59 @@ class Geocoder:
         self.__gebref = gebref
         self.__time = DateTime.now().strftime('%H:%M:%S')
 
-    # get Coordinates from an address
-    def getCoordinates(self, address):
+    # get coordinates from an address
+    def get_coordinates(self, address):
+        gebref = self.__search_address(address)
+
+        if gebref.empty == True:
+            return self.__geocode(address)
+
+        elif gebref.shape[0] == 1:
+            coordinates = gebref.to_dict(orient='list')
+            y = coordinates['east']
+            x = coordinates['north']
+            accuracy = 1
+
+            return self.__return_data(accuracy=accuracy,
+                                      x=x,
+                                      y=y)
+
+        else:
+            return self.__geocode(address)
+
+    def __search_address(self, address):
         a = address.split()
         counter = len(a)
 
         if counter == 2:
-            gebref = self.gebref >> mask(X.stn == a[0],
-                                         X.hsr == a[1])
+            gebref = self.__gebref >> mask(X.stn == a[0],
+                                           X.hsr == a[1])
+            return gebref
 
         elif counter == 3:
-            gebref = self.gebref >> mask(X.stn == a[0],
-                                         X.hsr == a[1],
-                                         X.adz == a[2])
+            gebref = self.__gebref >> mask(X.stn == a[0],
+                                           X.hsr == a[1],
+                                           X.adz == a[2])
+            return gebref
 
         else:
-            print("Geocode-function is used to get the coordinates..")
-            return self.geocode(address)
-
-            coordinates = gebref.to_dict(orient='list')
-            x = coordinates['east']
-            y = coordinates['north']
-            accuracy = 1
-
-            return {"accuracy": accuracy,
-                    "x": x,
-                    "y": y}
+            return self.__gebref
 
     def __geocode(self, address):
 
         try:
             # query the time
             current_time = DateTime.now().strftime('%H:%M:%S')
+
             if self.__time_difference(current_time) < 1:
-                time.sleep(1000)
+                 time.sleep(1000)
 
             # set time
             self.time(current_time)
 
             # geocode address
             g = geocoder.osm(address)
+
             if g.ok:
 
                 json = g.geojson['features'][0]['properties']
@@ -76,15 +90,15 @@ class Geocoder:
                 x_trans, y_trans = self.__wgs2etrs(lng, lat)
 
                 return self.__return_data(accuracy=accuracy,
-                                        x=x_trans,
-                                        y=y_trans)
+                                          x=x_trans,
+                                          y=y_trans)
             else:
                 return self.__return_data()
 
 
         except ConnectionError as e:
             print(e)
-            return self.return_data()
+            return self.__return_data()
 
     @staticmethod
     def __return_data(accuracy=0, x=0, y=0):
@@ -116,5 +130,7 @@ class Geocoder:
         return time - self.time
 
 if __name__ == '__main__':
-    # g = Geocoder(gebref=, adresse="Witteringstr 9")
-    pass
+    br = BuildingReferences(city='Essen',
+                            path='C:/gebref')
+    g = Geocoder(br.gebref)
+    print(g.get_coordinates("Witteringstr 9"))
