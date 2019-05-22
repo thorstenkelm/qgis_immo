d493ab4ce9a398d0d757b0c81b630de825bdfc9b
thorstenkelm <thorsten.kelm@hs-bochum.de>
2019-05-22 07:53:39 +0200

    Updated scrapper

--- RealEstate/ImmobilienScout24/ImmoScout24Scrapper.py
+++ RealEstate/ImmobilienScout24/ImmoScout24Scrapper.py
@@ -12,8 +12,8 @@ from datetime import datetime
 import json
 from flatten_dict import flatten
 
+from RealEstate.Geocoding.BuildingReferences import BuildingReferences
 from RealEstate.Geocoding.Geocoder import Geocoder
-
 from RealEstate.ImmobilienScout24.ImmoDataModel import ImmoDataModel
 
 
@@ -23,93 +23,92 @@ class ImmoScout24Scrapper:
 
     def __init__(self, url):
         self.url = url
-        print("Die AusgangsUrl lautet: "+self.url)
         self.city = self.get_city()
-        print("Es werden "+ str(self.get_numberOfPages())+" Seiten durchsucht")
+        self.number_of_pages = self.get_number_of_pages()
+        self.path = self.get_script_path()
+        self.gebref = BuildingReferences(city=self.city,
+                                         path=self.path)
+
+        print("Starting URL: "+self.url)
+        print("Number of pages: " + str(self.number_of_pages))
 
     def execute(self):
         """
         Execute method to start the functions for data scraping
-        :return pandas dataframe:
+        :return: Pandas DataFrame
         """
-        path = self.get_script_path()
+        return self.scrape()
 
-        return self.ImmoscoutScrape(memoryLocation=path + "\\")
-
-
-    def URLSplitter(self):
+    def url_splitter(self):
         """
+        Splits the URL and gathers information if it is a house or flat, if it is for rent or to buy
+        and in which state/city the property is
         :return list:
         """
-        L = (self.url.split('/'))
+        url_split = (self.url.split('/'))
 
-        if len(L) == 8:
-            a = L[5].split('-')
-            residence = a[0]
-            payment = a[1]
-            state = L[6]
-            city = L[7]
-            # splits the URL and gathers information if it is a house or flat, if it is for rent or to buy
-            # and in which state/city the property is
-            Liste = [residence, payment, state, city]
+        if len(url_split) == 8:
+            element = url_split[5].split('-')
+            residence = element[0]
+            payment = element[1]
+            state = url_split[6]
+            city = url_split[7]
 
-        # len(L)==9
         else:
-            a = L[5].split('-')
-            residence = a[0]
-            payment = a[1]
-            state = L[6] + "/" + L[7]
-            city = L[8]
-            # splits the URL and gathers information if it is a house or flat, if it is for rent or to buy
-            # and in which state/city the property is
-            Liste = [residence, payment, state, city]
-        return Liste
+            element = url_split[5].split('-')
+            residence = element[0]
+            payment = element[1]
+            state = url_split[6] + "/" + url_split[7]
+            city = url_split[8]
+
+        return {'residence': residence,
+                'payment': payment,
+                'state': state,
+                'city': city}
 
     def get_city(self):
         """
-        :return string:
+        Get city from url
+        :return: string
         """
-        return self.URLSplitter()[-1]
+        para = self.url_splitter()
+        return para['city']
 
-    def get_subpageUrl(self, property_id):
+    def build_url(self, expose_id):
         """
-        uses the returned main part of the URl and adds the
+        Uses the returned main part of the URl and adds the
         parts to create the URL for the single property
-        :param property_id:
-        :return string:
+        :return: string
         """
-
-        return self.IS24_URL + '/' + property_id
+        return self.IS24_URL + '/' + expose_id
 
     def get_soup(self):
         """
         Use of the BeautifulSoup4 package to get the source code of the URL
         :return soup:
         """
-
         try:
             return bs.BeautifulSoup(self.open_stream(),
-                                'lxml')
+                                    'lxml')
         except Exception as e:
-            print(str(datetime.now()) + ": " + str(e))
-
+            print("Get soup: ", str(datetime.now()) + ": " + str(e))
 
     def open_stream(self):
         """
-        :return bytes:
+        Open stream for bs4
+        :return: Request-Object
         """
-
         return urllib.request.urlopen(self.url).read()
 
-    def get_numberOfPages(self):
+    def get_number_of_pages(self):
         """
         Gets the number of all pages for the chosen city/place
-        :return int:
+        :return: int
         """
         entries = self.get_real_estate_entries(self.get_soup())
         return entries['paging']['numberOfPages']
 
-    def get_nextPage(self):
+    def get_next_page(self):
         """
         Searches in the source code for the href to the next page
         :return string:
@@ -118,64 +117,56 @@ class ImmoScout24Scrapper:
         return entries['paging']['next']['@xlink.href']
 
     @staticmethod
-    def get_keyValues(soup):
+    def get_key_values(soup):
         """
-        filters the important data out of the source code
-        :param soup:
-        :return dict:
+        Filters the important data out of the source code
+        :param: Soup
+        :return: Dict
         """
         paragraph = str(soup.find_all("script"))
         key_values = paragraph.split("keyValues = ")[1]
         key_values_json = json.loads(key_values.split("}")[0] + str("}"))
 
-        return pd.DataFrame(data=key_values_json,
-                            index=[str(datetime.now())])
+        return key_values_json
 
     @staticmethod
-    def get_description(data, soup):
+    def get_description(soup):
         """
         gets the description of each property
-        :param data:
-        :param property_id:
-        :param soup:
-        :return list:
+        :param: bs4
+        :param: Soup
+        :return: List
         """
         try:
+            # description is placed in the pre tag of the source code
             description = []
-            #description is placed in the pre tag of the source code
+
             for i in soup.find_all("pre"):
                 description.append(i.text)
-            data["description"] = str(description)
+
             return description
 
         except Exception as e:
-            print(str(datetime.now()) + ": " + str(e))
+            print("Get description: ", str(datetime.now()) + ": " + str(e))
 
-    @staticmethod
-    def DF2CSV(path, df):  # TODO ist der ausgabe pfad nicht als eingabe sinnvoll __init__?
-        """
-        function to transform a Pandas dataframe into a CSV
-        :param path:
-        :param df:
-        :return none:
-        """
-        print("Creating CSV")
-
+    def build_result_path(self):
         date = str(datetime.now())[:19].replace(":", "").replace(".", "")
-        path = path + date + ".csv"
+        return self.path + '-' + str(self.city) + '-' + date + '.csv'
 
-        df.to_csv(path,
+    def df2csv(self, df):
+        """
+        Function to transform a Pandas DataFrame into a CSV
+        """
+        df.to_csv(self.build_result_path(),
                   sep=';',
-                  decimal=',',
+                  decimal='.',
                   encoding='utf-8',
-                  date_label='timestamp')
+                  index=False)
 
     def get_real_estate_entries(self, soup):
         """
-        :param soup:
-        :return dict:
+        Get entries from bs4
         """
-
         # get all script elements
         for paragraph in soup.find_all("script"):
             # get exposes script element
@@ -187,7 +178,6 @@ class ImmoScout24Scrapper:
                     if line.strip().startswith('resultListModel'):
                         # convert str to json
                         entries = self.str_to_json(string=line)
-                        #entries = flatten(entries)
                         return entries
         return None
 
@@ -195,120 +185,120 @@ class ImmoScout24Scrapper:
     def str_to_json(string):
         """
         Transforms string to JSON
-        :param string:
-        :return dict:
         """
         string = string.strip()
         string_json = json.loads((string[17:-1]))
         return string_json["searchResponseModel"]["resultlist.resultlist"]
 
-    def ImmoscoutScrape(self, memoryLocation: str):
+    def scrape(self):
         """
         function to run through all pages and get data from each property
-        :param memoryLocation:
-        :return pandas dataframe:
         """
-        all_pages = self.get_numberOfPages()
-        current_page = 1
+        try:
+            current_page = 1
 
-        # geocoder for geocoding addresses
-        g = Geocoder()
+            # geocoder for geocoding addresses
+            g = Geocoder(self.gebref)
 
-        # daten container fuer alle datensaetze
-        all_data=pd.DataFrame()
-        df = pd.DataFrame()
-        data=pd.DataFrame()
-        # stops at last page
-        while (current_page <= all_pages):
+            # data container
+            df = pd.DataFrame()
 
+            # stops at last page
+            while current_page <= self.number_of_pages:
+                print('Page: ', current_page)
 
-            try:
                 # gathers the information out of the soup object for each property entry
                 entries_information = self.get_real_estate_entries(self.get_soup())
-                #return entries_information['paging', 'next', '@xlink.href']
 
                 # filters all given information for relevant data
-                resultEntry = entries_information['resultlistEntries'][0]['resultlistEntry']
+                result_entry = entries_information['resultlistEntries'][0]['resultlistEntry']
 
-                for property_main_page in resultEntry:
+                # iteration of exposes
+                for property_main_page in result_entry:
 
                     property_id = property_main_page['@id']
+                    print("Expose: ", property_id)
 
                     property_main_page = flatten(d=property_main_page,
-                                              reducer=self.underscore_reducer)
+                                                 reducer=self.underscore_reducer)
 
                     # elements of sub page
-                    property_sub_page = self.get_subpage(property_id, df)
+                    property_sub_page = self.get_sub_page(property_id)
                     property_sub_page = flatten(d=property_sub_page,
-                                              reducer=self.underscore_reducer)
+                                                reducer=self.underscore_reducer)
 
-                    # connect data from sub page with main page data
-                    # achtung nestes dict -> flatten
+                    # add extraction time
+                    property_sub_page['extraction_date'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+                    # merge data
                     data_property = {**property_main_page, **property_sub_page}
-
 
                     mod = ImmoDataModel(data_property)
 
-                    # geocode address and set coordinates
-                    coord = g.geocode(mod.get_address())
-                    mod.set_coordinates(coordinates=coord)
+                    precise_house_number = mod.get_value('precise_house_number')
+                    mod_coord = mod.check_coordinates()
 
+                    if precise_house_number:
 
+                        # get address
+                        address = mod.get_address()
 
-                    #inserat = inserat.append(property_sub_page)
-                    #print(property_id)
-                    data = data.append(mod.get_data(),
-                                       sort=True)
+                        # geocode address and set coordinates
+                        coord = g.get_coordinates(street=address['street'],
+                                                  street_house_number=address['street_house_number'],
+                                                  house_number_supplement=address['house_number_supplement'],
+                                                  city=address['city'],
+                                                  coord_available=mod_coord)
 
-                # add everything filtered into the dataframe
-                # daten alle zu einem Datensatz zusammenfuehren
-                # df = df.append(data, sort=True)
-                # convert dataframe to CSV and save it to the chosen location
-                self.DF2CSV(memoryLocation, data)
+                        if coord:
+                            mod.set_coordinates(coordinates=coord)
+
+                    # add data to result
+                    df = df.append(mod.get_data)
 
                 # set Url to the next page to check
-                next_page = self.get_nextPage()
+                next_page = self.get_next_page()
 
                 self.url = self.IS24_URL + next_page
 
+                # increment page
+                current_page = current_page + 100
 
-            except Exception as e:
-                print(str(datetime.now()) + ": " + str(e))
+            return df
 
-            # increment page
-            current_page = current_page + 1
-            all_data=all_data.append(data)
+        except Exception as e:
+            print('Scrapper: ', str(datetime.now()) + ": " + str(e))
 
-        return all_data
-    def get_subpage(self, property_id, df):
+    def get_sub_page(self, property_id):
         """
         Method to extract KeyValues(relevant data) out of the script tags for each property
-        :param property_id:
-        :param df:
-        :return DataFrame:
         """
-
         try:
-            url_subpage = self.get_subpageUrl(property_id)  # english and underscore
-
+            # build and store url
+            url_sub_page = self.build_url(property_id)  # english and underscore
             url_h = self.url
 
-            self.url=url_subpage
+            self.url = url_sub_page
+            soup = self.get_soup()
 
-            soup = self.get_soup() # http mit in die url builder methode packen
-            # Key Value Pairs aus HTMl ziehen und in JSON speichern
-
+            # restore url
             self.url = url_h
-            data = self.get_keyValues(soup)
-            self.get_description(data, soup)  # TODO objektbeschreibung, ausstattung und lage enthalten? ja ist enthalten
-            df = df.append(data, sort=True)
-            return df
+
+            # extract data
+            data = self.get_key_values(soup)
+
+            # data['description'] = self.get_description(data)
+
+            return data
 
         except Exception as e:
             print(str(datetime.now()) + ": " + str(e))
 
     @staticmethod
     def underscore_reducer(k1, k2):
+        """
+        Method to flatten dict by underscore
+        """
         if k1 is None:
             return k2
         else:
@@ -318,15 +308,14 @@ class ImmoScout24Scrapper:
     def get_script_path():
         """
         Function to set storage location to the location of the script file
-        :return file Directory as string:
+        :return: file Directory as string
         """
         return os.path.dirname(os.path.abspath(__file__))
 
 
-
 if __name__ == "__main__":
     # single property
-    test_url = 'https://www.immobilienscout24.de/Suche/S-2/P-1/Wohnung-Miete/Rheinland-Pfalz/Zweibruecken/Ixheim'
+    # test_url = 'https://www.immobilienscout24.de/Suche/S-2/P-1/Wohnung-Miete/Rheinland-Pfalz/Zweibruecken/Ixheim'
 
     # url studyarea
     url = 'https://www.immobilienscout24.de/Suche/S-T/Wohnung-Miete/Nordrhein-Westfalen/Essen'
@@ -335,4 +324,6 @@ if __name__ == "__main__":
     my_Scraper = ImmoScout24Scrapper(url=url)
 
     # Run main method
-my_Scraper.execute()
+    data = my_Scraper.execute()
+
+    my_Scraper.df2csv(data)
