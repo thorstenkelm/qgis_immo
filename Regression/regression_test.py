5f4977a22fc8c1944d95a72e815fde55214a2a68
marlh <marlena.hecker@hs-bochum.de>
2019-06-28 13:24:40 +0200

    Added a function for splitting the data in training and test data, added a rmse validation function

--- Regression/regression_test.py
+++ Regression/regression_test.py
@@ -5,6 +5,8 @@ from dfply import *
 import matplotlib.pyplot as plt  # To visualize
 import seaborn as sns
 import statsmodels.formula.api as smf
+# Tool for training and test splitting
+from sklearn.model_selection import train_test_split
 
 dat = pd.read_csv("C:/Users/Kelm/Desktop/Daten/Mieten_Essen_Export.csv", sep=",", na_values=0)
 
@@ -12,6 +14,9 @@ df = dat >> select(X.Parkplaetze, X.Kaltmiete, X.AnzZimmer, X.Quartier, X.Wohnfl
      filter_by(X.Parkplaetze > 0, X.Kaltmiete > 0, X.AnzZimmer > 0)
 
 df['Quartier'] = pd.Categorical(df.Quartier)
+
+#training and test split, random_State makes the training and test split reproducible
+df_train, df_test = train_test_split(df,test_size=0.2,random_state=24, shuffle=True)
 
 est = smf.ols(formula="Kaltmiete ~ Quartier + AnzZimmer + Wohnflaeche", data=df).fit()
 print(est.summary())
@@ -42,3 +47,17 @@ plot_lm_1.axes[0].set_title('Residuals vs Fitted')
 plot_lm_1.axes[0].set_xlabel('Fitted values')
 plot_lm_1.axes[0].set_ylabel('Residuals')
 plot_lm_1.show()
+
+
+# Model validation on the test data (df_test)
+def rmse(predictions, targets):
+    diff = predictions - targets
+    diff_squared = diff ** 2
+    mean_of_diff_squared = diff_squared.mean()
+    rmse_val = np.sqrt(mean_of_diff_squared)
+
+    return rmse_val
+
+print("RMSE of the training dataset:" + str(rmse(predictions=est.predict(df_train), targets=df_train["Kaltmiete"])))
+print("RMSE of the test dataset:" + str(rmse(predictions=est.predict(df_test), targets=df_test["Kaltmiete"])))
+
